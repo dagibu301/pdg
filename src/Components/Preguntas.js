a75a3ca29dbe28400d92e66d39de2a7369e0c0c0
David Giraldo <davidgiraldobueno@gmail.com>
2020-01-06 18:18:27 -0500

    preguntas

--- src/Components/Preguntas.js
+++ src/Components/Preguntas.js
@@ -7,6 +7,54 @@ const preguntas = [
          "Hombre Trans"
      ] },
 
+     {enunciado: "¿Qué edad tienes?",
+     respuestas: [ 
+         "Entre 15-18",
+         "Entre 19-24",
+         "Entre 25-34",
+         "Entre 34-44"
+     ] },
+
+     {enunciado: "Cuando transitas por el espacio público ¿Te silban, te tiran besos, te dicen piropos, te persiguen o te bloquean el paso??",
+     respuestas: [ 
+         "Si, todos los días",
+         "Si, cada dos o tres días",
+         "Si, una vez por semana",
+         "Si, rara vez"
+     ] },
+
+     {enunciado: "Cuando a una mujer le silban, le tiran besos, le dicen piropos, la persiguen o le bloquean el paso, mientras transita por el espacio público ¿Qué es lo primero que piensas?",
+     respuestas: [ 
+         "Es normal, porque está muy bonita",
+         "Debe sentirse halagada por recibir tanta atención",
+         "Si se viste así es porque quiere recibir piropos",
+         "Debe sentirse incómoda recibiendo piropos de un extraño",
+         "Es una falta de respeto, las mujeres no deberían aguantar piropos en la calle"
+     ] },
+
+     {enunciado: "",
+     respuestas: [ 
+         "",
+         "",
+         "",
+         ""
+     ] },
+
+     {enunciado: "",
+     respuestas: [ 
+         "",
+         "",
+         "",
+         ""
+     ] },
+
+     {enunciado: "",
+     respuestas: [ 
+         "",
+         "",
+         "",
+         ""
+     ] },
   ];
 
   export default preguntas;
