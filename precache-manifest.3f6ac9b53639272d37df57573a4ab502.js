af54a677a45092275321988bbfbe8b196f0ee691
David Giraldo <davidgiraldobueno@gmail.com>
2020-01-06 19:41:44 -0500

    Updates

---
+++ precache-manifest.3f6ac9b53639272d37df57573a4ab502.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2df8553f0bae123683885259f5158eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "124ff0c39cac6aa9e421",
+    "url": "/static/css/main.9905a726.chunk.css"
+  },
+  {
+    "revision": "6f1a19d6f2b094b58a1a",
+    "url": "/static/js/2.7cbdcf62.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7cbdcf62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "124ff0c39cac6aa9e421",
+    "url": "/static/js/main.7f102df2.chunk.js"
+  },
+  {
+    "revision": "f2e495f2be08c6cbd44f",
+    "url": "/static/js/runtime-main.62581e91.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "1068a0ceb61a76a4ca9f6cd50b937595",
+    "url": "/static/media/beep.1068a0ce.wav"
+  },
+  {
+    "revision": "dbd6960a4619ad1441d02652c5462b1c",
+    "url": "/static/media/square.dbd6960a.PNG"
+  },
+  {
+    "revision": "c40837a53db1af209f6cb7a1a76f0712",
+    "url": "/static/media/step1.c40837a5.PNG"
+  }
+]);
